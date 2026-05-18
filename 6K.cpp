65b3e6617de3cc9d7865684798a7fedcda241546
Maxim Veselov <m2veselov@gmail.com>
2026-05-19 00:36:34 +0300

    Update 6K.cpp

--- 6K.cpp
+++ 6K.cpp
@@ -3,12 +3,12 @@
 #include <limits>
 #include <vector>
 
-long long BinarySearch(const std::vector<long long>& d, long long x) {
+long long BinarySearch(const std::vector<long long>& array, long long target) {
   long long low = 0;
-  long long high = static_cast<long long>(d.size()) - 1;
+  long long high = static_cast<long long>(array.size()) - 1;
   while (low < high) {
     long long mid = (low + high) / 2;
-    if (d[mid] <= x) {
+    if (array[mid] <= target) {
       low = mid + 1;
     } else {
       high = mid;
@@ -20,35 +20,35 @@ long long BinarySearch(const std::vector<long long>& d, long long x) {
 int main() {
   long long n;
   std::cin >> n;
-  std::vector<long long> a(n);
+  std::vector<long long> sequence(n);
   for (long long i = 0; i < n; i++) {
-    std::cin >> a[i];
+    std::cin >> sequence[i];
   }
-  reverse(a.begin(), a.end());
-  std::vector<long long> d(n + 1);
+  reverse(sequence.begin(), sequence.end());
+  std::vector<long long> subseq(n + 1);
   std::vector<long long> pos(n + 1, -1);
   std::vector<long long> prev(n);
   long long length = 0;
 
   pos[0] = -1;
-  d[0] = std::numeric_limits<long long>::min();
+  subseq[0] = std::numeric_limits<long long>::min();
   for (long long i = 1; i < n + 1; i++) {
-    d[i] = std::numeric_limits<long long>::max();
+    subseq[i] = std::numeric_limits<long long>::max();
   }
   for (long long i = 0; i < n; i++) {
-    long long j = BinarySearch(d, a[i]);
-    if (d[j - 1] <= a[i] and a[i] <= d[j]) {
-      d[j] = a[i];
+    long long j = BinarySearch(subseq, sequence[i]);
+    if (subseq[j - 1] <= sequence[i] and sequence[i] <= subseq[j]) {
+      subseq[j] = sequence[i];
       pos[j] = i;
       prev[i] = pos[j - 1];
       length = std::max(length, j);
     }
   }
   std::vector<long long> answer;
-  long long p = pos[length];
-  while (p != -1) {
-    answer.push_back(n - p);
-    p = prev[p];
+  long long cur = pos[length];
+  while (cur != -1) {
+    answer.push_back(n - cur);
+    cur = prev[cur];
   }
   std::cout << answer.size() << '\n';
   for (long long i : answer) {
