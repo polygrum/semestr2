254b35dedcce261a2ef60a2f0588354a823bcb0e
Maxim Veselov <m2veselov@gmail.com>
2026-05-19 00:44:55 +0300

    Update 9F.cpp
    Не понял, к чему претензии. Вроде, везде обозначал размер графа за v_, v1 и v2 как то по другому обозначать странно, это просто две вершины

--- 9F.cpp
+++ 9F.cpp
@@ -5,27 +5,27 @@
 #include <vector>
 
 class Graph {
-  int v_;
+  int sz_;
   std::vector<std::vector<long long>> cap_;
   std::vector<std::vector<int>> index_;
 
  public:
-  explicit Graph(int v) : v_(v) {
-    cap_.assign(v, std::vector(v, 0LL));
-    index_.assign(v, std::vector(v, 0));
+  explicit Graph(int sz) : sz_(sz) {
+    cap_.assign(sz, std::vector(sz, 0LL));
+    index_.assign(sz, std::vector(sz, 0));
   }
 
-  void AddEdge(int v1, int v2, int c, int index) {
-    cap_[--v1][--v2] += c;
-    cap_[v2][v1] += c;
-    index_[v1][v2] = index;
-    index_[v2][v1] = index;
+  void AddEdge(int u, int v, int cap, int index) {
+    cap_[--u][--v] += cap;
+    cap_[v][u] += cap;
+    index_[u][v] = index;
+    index_[v][u] = index;
   }
 
   auto EdmondsKarp() {
     auto res = cap_;
 
-    std::vector parent(v_, -1);
+    std::vector parent(sz_, -1);
     while (true) {
       std::fill(parent.begin(), parent.end(), -1);
 
@@ -33,29 +33,29 @@ class Graph {
       q.push(0);
       parent[0] = 0;
 
-      while (!q.empty() && parent[v_ - 1] == -1) {
+      while (!q.empty() && parent[sz_ - 1] == -1) {
         int v = q.front();
         q.pop();
-        for (int i = 0; i < v_; i++) {
+        for (int i = 0; i < sz_; i++) {
           if (res[v][i] > 0 && parent[i] == -1) {
             parent[i] = v;
             q.push(i);
           }
         }
       }
-      if (parent[v_ - 1] == -1) {
-        std::vector<bool> is_left(v_);
-        for (int i = 0; i < v_; i++) {
+      if (parent[sz_ - 1] == -1) {
+        std::vector<bool> is_left(sz_);
+        for (int i = 0; i < sz_; i++) {
           is_left[i] = parent[i] != -1;
         }
         return is_left;
       }
 
       long long path_flow = std::numeric_limits<long long>::max();
-      for (auto i = v_ - 1; i != 0; i = parent[i]) {
+      for (auto i = sz_ - 1; i != 0; i = parent[i]) {
         path_flow = std::min(path_flow, res[parent[i]][i]);
       }
-      for (auto i = v_ - 1; i != 0; i = parent[i]) {
+      for (auto i = sz_ - 1; i != 0; i = parent[i]) {
         res[parent[i]][i] -= path_flow;
         res[i][parent[i]] += path_flow;
       }
@@ -65,8 +65,8 @@ class Graph {
   void FindCut(const std::vector<bool>& is_left) {
     long long cut_sum = 0;
     std::vector<int> cut;
-    for (int v = 0; v < v_; v++) {
-      for (int u = 0; u < v_; u++) {
+    for (int v = 0; v < sz_; v++) {
+      for (int u = 0; u < sz_; u++) {
         if (cap_[v][u] != 0 && is_left[v] && !is_left[u]) {
           cut_sum += cap_[v][u];
           cut.push_back(index_[v][u]);
@@ -87,11 +87,11 @@ int main() {
   std::cin >> n >> m;
   Graph g(n);
   for (int i = 0; i < m; i++) {
-    int v1;
-    int v2;
+    int u;
+    int v;
     int c;
-    std::cin >> v1 >> v2 >> c;
-    g.AddEdge(v1, v2, c, i + 1);
+    std::cin >> u >> v >> c;
+    g.AddEdge(u, v, c, i + 1);
   }
   g.FindCut(g.EdmondsKarp());
 }
